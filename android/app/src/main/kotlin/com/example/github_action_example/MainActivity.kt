e2eb67e7d65fa9e2a579355f300980ffbd8c645b
EdsonMello-code <edsonmelo754@gmail.com>
2022-08-07 20:34:39 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/github_action_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.github_action_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:github_action_example/app_widget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
